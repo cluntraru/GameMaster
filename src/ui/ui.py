5da53e22d6cf6b127c6a8eadcb2fb5c678727dfa
dobrebogdan <36445756+dobrebogdan@users.noreply.github.com>
2019-02-24 20:02:15 +0200

    Added ui.py with assassin and cop functionality.

---
+++ src/ui/ui.py
@@ -0,0 +1,93 @@
+from tkinter import *
+
+COLORS=["red","green","blue","brown","orange","purple"]
+NOBODY = "NONE"
+
+assassinatedPerson = NOBODY
+checkedPerson = NOBODY
+
+def createVotingScreen(player_window, player_names, vote_function): #populates all voting screens
+
+    top_frame = Frame(player_window)
+    top_frame.pack()
+
+    bottom_frame = Frame(player_window)
+    bottom_frame.pack()
+
+    numberOfPlayers = len(player_names)
+    for i in range(0, numberOfPlayers):
+        playerName = player_names[i]
+        if (i < numberOfPlayers / 2):
+            currFrame = top_frame
+        else:
+            currFrame = bottom_frame
+        b = Button(currFrame, fg=COLORS[i % len(COLORS)], height=20, width=17,
+                   text=playerName, command=vote_function(player_window, playerName))
+        b.pack(side=LEFT)
+
+    player_window.mainloop()  # make sure buttons are constantly displayed
+
+def dayVote(player_names):
+    playerVotes = {}
+    for playerName in player_names:
+        playerVotes[playerName] = 0
+    playerVotes[NOBODY] = 0
+    for playerName in player_names:
+
+        currWindow = Tk()
+        currPlayer = playerName
+        currWindow.title("DAY PHASE: " + currPlayer + " votes ")
+
+        def dayVoteFunction(playerWindow,playerName):
+            def callback():
+                playerVotes[playerName] += 1
+                playerWindow.destroy()
+            return callback
+
+        createVotingScreen(currWindow, player_names, dayVoteFunction)
+
+
+    hangedPlayer = NOBODY
+    for playerName in player_names:
+        if (playerVotes[playerName] > len(player_names)/2):
+            hangedPlayer = playerName
+    print("The victim was: " + hangedPlayer)
+    return hangedPlayer
+
+def nightAssassinVote(town_names):
+    currWindow = Tk()
+    currWindow.title("NIGHT PHASE: " +  "Assassins kill: ")
+
+    def assassinVoteFunction(playerWindow, playerName):
+        def callback():
+            global assassinatedPerson
+            assassinatedPerson = playerName
+            playerWindow.destroy()
+
+        return callback
+
+    createVotingScreen(currWindow, town_names, assassinVoteFunction)
+
+    global assassinatedPerson
+    return assassinatedPerson
+
+def nightCopVote(player_names):
+    currWindow = Tk()
+    currWindow.title("NIGHT PHASE: " +  "Assassins kill: ")
+
+    def copVoteFunction(playerWindow, playerName):
+        def callback():
+            global checkedPerson
+            checkedPerson = playerName
+            playerWindow.destroy()
+
+        return callback
+
+    createVotingScreen(currWindow, player_names, copVoteFunction)
+
+    global checkedPerson
+    return checkedPerson
+
+
+
+nightCopVote(["Marcel","Ionela","Swagmaster","Bozgorel","Atcineva"])

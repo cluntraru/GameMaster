aaf665af7c6a49f7a34c7ce0171b4e58438d737e
Chris Luntraru <chris.luntraru@gmail.com>
2019-02-24 16:32:49 +0200

    Refactoring for coding style

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+''' Implementation of the storyteller in the popular game 'Mafia' '''
+
 import random
 import math
 import sys
@@ -13,9 +15,8 @@ DOCTOR_IDX = 4
 MTLT_IDX = 5
 POTATO_IDX = 6
 
-cycle_count = 1
-debug_mode = None
-speak_mode = None
+DEBUG_MODE = None
+SPEAK_MODE = None
 suicidal_lynched = False
 
 roles_cnt = [0, 0, 0, 0, 0, 0, 0]
@@ -25,7 +26,11 @@ player_roles = {}
 player_alive = {}
 
 def output(text):
-    if speak_mode:
+    ''' Speaks and prints the input text if SPEAK_MODE is enabled. Prints
+    otherwise
+    '''
+
+    if SPEAK_MODE:
         speak(text)
         print(text)
     else:
@@ -33,16 +38,25 @@ def output(text):
 
 
 def speak(text):
+    ''' Wrapper for platform specific text-to-speech functionality. Should be
+    called similar to print().
+    '''
+
+    # TODO: (Chris) add text-to-speech for Windows and Linux
     if sys.platform == 'darwin':
         os.system('say ' + text)
 
 
 def assign_roles():
+    ''' Randomly generates roles for each of the players. Also asks for number
+    of players and names.
+    '''
+
     # TODO: (Lulu) UI for entering names
     roles_cnt[PLAYER_IDX] = int(input('Enter number of players: '))
     if roles_cnt[PLAYER_IDX] < 5:
         output('Unfortunately, you can\'t play with less than 5 people :(')
-        return -1;
+        return -1
 
     roles_cnt[ASSN_IDX] = math.floor(1 + (roles_cnt[PLAYER_IDX] - 5) / 4)
     roles_cnt[POLICE_IDX] = math.floor(1 + (roles_cnt[PLAYER_IDX] - 5) / 5)
@@ -82,58 +96,73 @@ def assign_roles():
             curr_name = input('Please choose another name: ')
 
         rand_index = random.randint(0, len(role_list) - 1)
-        player_roles[curr_name] = role_list.pop(rand_index);
+        player_roles[curr_name] = role_list.pop(rand_index)
         player_alive[curr_name] = True
 
-    # print('\n')
+    print('\n')
     return 0
     # TODO: (Lulu) Make a UI so that everyone can find out their roles kthxbye
 
 
-def kill(name):
-    player_alive[name] = False
-    alive_cnt[player_roles[name]] -= 1
+def kill(player_name):
+    ''' Removes a player from the game, updating all the necessary structures.
+    '''
+
+    player_alive[player_name] = False
+    alive_cnt[player_roles[player_name]] -= 1
     alive_cnt[PLAYER_IDX] -= 1
 
 
 def role_idx_to_name(idx):
+    ''' Converts a role index constant to the actual role name.'''
+
     if idx == 0:
-        if debug_mode:
+        if DEBUG_MODE:
             print('DEBUG: Invalid role index')
         sys.exit()
 
-    role_names = ['ERROR', 'assassin', 'policeman', 'suicidal person', 'doctor', 'mutilator', 'townie']
+    role_names = ['ERROR', 'assassin', 'policeman', 'suicidal person',\
+                  'doctor', 'mutilator', 'townie']
     return role_names[idx]
 
 
 def mafia_won():
+    ''' True if mafia satisfies their win condition, False otherwise. '''
     return alive_cnt[ASSN_IDX] > alive_cnt[PLAYER_IDX] - alive_cnt[ASSN_IDX]
 
 
 def town_won():
+    ''' True if town satisfies their win condition, False otherwise. '''
     return alive_cnt[ASSN_IDX] == 0
 
 
 def suicidal_won():
+    ''' True if the suicidal person satisfies their win condition, False
+    otherwise.
+    '''
     return suicidal_lynched
 
 
 def game_over():
+    ''' True if any of the factions satisfy their win condition.
+    '''
     return town_won() or mafia_won() or suicidal_won()
 
 
-def play_day():
+def play_day(cycle_count):
+    ''' Simulates the next daytime phase in the game. '''
+
     # TODO: (Lulu) Make a UI for Lynching (lynched_name is name of victim)
     still_alive = []
-    for name in player_alive:
-        if player_alive[name]:
-            still_alive.append(name)
+    for player_name in player_alive:
+        if player_alive[player_name]:
+            still_alive.append(player_name)
 
     print('Still alive: '  + str(still_alive))
 
     lynched_name = input('Name of lynched player: ')
     if lynched_name != 'NONE':
-        while lynched_name not in player_roles or player_alive[lynched_name] == False:
+        while lynched_name not in player_roles or not player_alive[lynched_name]:
             lynched_name = input('Not a valid player.\nName of lynched player: ')
 
         if player_roles[lynched_name] == SUICD_IDX:
@@ -145,23 +174,13 @@ def play_day():
     print('\n---------- DAY ' + str(cycle_count) + ' END ----------\n')
 
 
-def valid_target(name):
-    # Checks if person is in the game and alive
-    return name in player_roles and player_alive[name]
+def valid_target(player_name):
+    ''' Checks if a player is in the game and alive. '''
+    return player_name in player_roles and player_alive[player_name]
 
 
-def play_night():
-    # These symbolise if the respective turns can still act at night
-    assn_turn = bool(alive_cnt[ASSN_IDX])
-    police_turn = bool(alive_cnt[POLICE_IDX])
-    doctor_turn = bool(alive_cnt[DOCTOR_IDX])
-    mutilator_turn = bool(alive_cnt[MTLT_IDX])
-
-    # TODO: (Lulu) UI for every night action
-    print('---------- NIGHT ' + str(cycle_count) + ' ----------\n')
-    output('Everyone goes to sleep.\n')
-
-    # Assn
+def assn_night(assn_turn):
+    ''' Simulates the assassins' night phase '''
     output('The assassins wake up.')
     if assn_turn:
         assassinated = input('Person to assassinate: ')
@@ -175,8 +194,11 @@ def play_night():
         time.sleep(sleep_time)
 
     output('The assassins go to sleep.\n')
+    return assassinated
 
-    # Police
+
+def police_night(police_turn):
+    ''' Simulates the police's night phase '''
     output('The police wake up.')
     if police_turn:
         police_query = input('Person to query: ')
@@ -186,7 +208,7 @@ def play_night():
         police_query = None
         sleep_time = random.randint(6, 10)
         time.sleep(sleep_time)
-    
+
     if police_query and player_roles[police_query] == ASSN_IDX:
         print('The person you queried is an assassin.\n')
     elif police_query:
@@ -194,15 +216,17 @@ def play_night():
 
     output('The police go to sleep.\n')
 
-    # Mutilator
+
+def mutilator_night(mutilator_turn):
+    '''' Simulates the mutilators' night phase. '''
     output('The mutilators wake up.')
     if mutilator_turn:
         mutilated = input('Person to mutilate: ')
         while not valid_target(mutilated):
-            mutilated = input('Invalid target. Person to mutilate: ') 
+            mutilated = input('Invalid target. Person to mutilate: ')
 
         mutilated_area = input('Area to mutilate (m/h): ')
-        while mutilated_area != 'm' and mutilated_area != 'h':
+        while mutilated_area not in ('m', 'h'):
             mutilated_area = input('Invalid area. Choose \'m\' for mouth or ' + \
                                    '\'h\' for hand: ')
     else:
@@ -212,8 +236,11 @@ def play_night():
         time.sleep(sleep_time)
 
     output('The mutilators go to sleep.\n')
+    return mutilated, mutilated_area
 
-    # Doctor
+
+def doctor_night(doctor_turn):
+    ''' Simulates the doctors' night phase. '''
     output('The doctors wake up.')
     if doctor_turn:
         patient = input('Person to protect: ')
@@ -224,13 +251,33 @@ def play_night():
         sleep_time = random.randint(6, 10)
         time.sleep(sleep_time)
 
+    output('The doctors go to sleep.\n')
+    return patient
+
+
+def play_night(cycle_count):
+    ''' Simulates the next night phase in the game. '''
+
+    # These symbolise if the respective turns can still act at night
+    assn_turn = bool(alive_cnt[ASSN_IDX])
+    police_turn = bool(alive_cnt[POLICE_IDX])
+    doctor_turn = bool(alive_cnt[DOCTOR_IDX])
+    mutilator_turn = bool(alive_cnt[MTLT_IDX])
+
+    # TODO: (Lulu) UI for every night action
+    print('---------- NIGHT ' + str(cycle_count) + ' ----------\n')
+    output('Everyone goes to sleep.\n')
+
+    assassinated = assn_night(assn_turn)
+    police_night(police_turn)
+    mutilated, mutilated_area = mutilator_night(mutilator_turn)
+    patient = doctor_night(doctor_turn)
+
     if patient and patient == mutilated:
         mutilated = None
 
     if patient and patient == assassinated:
         assassinated = None
-
-    output('The doctors go to sleep.\n')
 
     print('---------- NIGHT ' + str(cycle_count) + ' END ----------\n')
     print('---------- DAY ' + str(cycle_count + 1) + ' ----------\n')
@@ -255,50 +302,54 @@ def play_night():
     print('\n')
 
 
-def print_player(name):
-    if player_alive[name]:
+def print_player(player_name):
+    ''' Prints the role and status (dead/alive) of a player. '''
+    if player_alive[player_name]:
         status = 'ALIVE'
     else:
         status = 'DEAD'
 
-    print(name + ' the ' + role_idx_to_name(player_roles[name]) + ' - ' + \
+    print(player_name + ' the ' + role_idx_to_name(player_roles[player_name]) + ' - ' + \
           status)
 
 
 def print_mafia():
+    ''' Prints all members of the mafia. '''
     print('Mafia:')
-    for name in player_roles:
-        if player_roles[name] != ASSN_IDX:
+    for player_name in player_roles:
+        if player_roles[player_name] != ASSN_IDX:
             continue
 
-        print_player(name)
+        print_player(player_name)
 
     print('\n')
 
 
 def print_town():
+    ''' Prints all members of the town. '''
     print('Town:')
-    for name in player_roles:
-        if player_roles[name] == ASSN_IDX or player_roles[name] == SUICD_IDX:
+    for player_name in player_roles:
+        if player_roles[player_name] == ASSN_IDX or player_roles[player_name] == SUICD_IDX:
             continue
 
-        print_player(name)
+        print_player(player_name)
 
     print('\n')
 
 
 def print_suicidal():
+    ''' Prints the suicidal person. '''
     print('Neutral:')
-    for name in player_roles:
-        if player_roles[name] == SUICD_IDX:
-            print_player(name)
+    for player_name in player_roles:
+        if player_roles[player_name] == SUICD_IDX:
+            print_player(player_name)
             break
 
     print('\n')
 
 
 def print_results():
-    # TODO: (Chris) print results
+    ''' Prints the scoreboard. '''
     print('---------- RESULTS ----------')
     if suicidal_won():
         output("The suicidal person won!\n")
@@ -316,41 +367,40 @@ def print_results():
 
 
 def play_game():
+    ''' Runs the simulation. '''
+    cycle_count = 1
     print('---------- DAY 1 ----------\n')
     while not game_over():
-        play_day()
+        play_day(cycle_count)
         if game_over():
             return
 
-        play_night()
-
-        global cycle_count
+        play_night(cycle_count)
         cycle_count += 1
 
 
 # Execution starts here
 parser = ArgumentParser()
-parser.add_argument('-x', '--textonly', action = 'store_true',
-                    help = 'Do not use voice commands')
+parser.add_argument('-x', '--textonly', action='store_true',
+                    help='Do not use voice commands')
 
-parser.add_argument('-d', '--debug', action = 'store_true',
-                    help = 'All input comes from console')
+parser.add_argument('-d', '--debug', action='store_true',
+                    help='All input comes from console')
 
-parser.add_argument('-t', '--test', action = 'store_true',
-                    help = 'Run tests')
+parser.add_argument('-t', '--test', action='store_true', help='Run tests')
 
 args = parser.parse_args()
-debug_mode = args.debug
-test_mode = args.test
-speak_mode = not args.textonly
+DEBUG_MODE = args.debug
+TEST_MODE = args.test
+SPEAK_MODE = not args.textonly
 
 if assign_roles():
     sys.exit()
 
-if debug_mode:
+if DEBUG_MODE:
     # Print all the roles
-    for name in player_roles:
-        print(name + ' the ' + role_idx_to_name(player_roles[name]) + '.')
+    for dbg_name in player_roles:
+        print(dbg_name + ' the ' + role_idx_to_name(player_roles[dbg_name]) + '.')
 
 play_game()
 print_results()
